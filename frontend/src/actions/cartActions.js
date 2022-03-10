7731f47e34fb46d67ca3609967821475094c3f80
KaranBhut44 <karan.bhut@students.iiit.ac.in>
2022-03-10 16:12:59 +0530

    Created Place order screen

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -20,7 +20,7 @@ export const addToCart = (id, qty) => async (dispatch, getState) => {
       qty,
     },
   })
-  localStorage.seItem("cartItems", JSON.stringify(getState().cart.cartItems))
+  localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems))
 }
 
 export const removeFromCart = (id) => (dispatch, getState) => {
