28bbf0c7f2bda2efa2822ba18e2ad9961487cfaf
KaranBhut44 <karan.bhut@students.iiit.ac.in>
2022-03-10 16:24:01 +0530

    Added order controller and route

--- frontend/src/screens/PlaceOrderScreen.js
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -1,14 +1,6 @@
 import React, { useState } from "react"
 import { Link } from "react-router-dom"
-import {
-  Button,
-  Row,
-  Col,
-  ListGroup,
-  Image,
-  Card,
-  ListGroupItem,
-} from "react-bootstrap"
+import { Button, Row, Col, ListGroup, Image, Card } from "react-bootstrap"
 import { useDispatch, useSelector } from "react-redux"
 import Message from "../components/Message"
 import CheckoutSteps from "../components/CheckoutSteps"
