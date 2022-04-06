cebfb0749f4a457d5aae426c678f8e1914716fc8
KaranBhut44 <karan.bhut@students.iiit.ac.in>
2022-04-06 16:06:39 +0530

    Added functionality for admin to see orders

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -84,4 +84,18 @@ const getMyOrders = asyncHandler(async (req, res) => {
   res.json(orders)
 })
 
-export { addOrderItems, getOrderById, updateOrderToPaid, getMyOrders }
+// @desc Get all user orders
+// @route GET /api/orders/myorders
+// @access Private/Admin
+const getOrders = asyncHandler(async (req, res) => {
+  const orders = await Order.find({}).populate("user", "id name")
+  res.json(orders)
+})
+
+export {
+  addOrderItems,
+  getOrderById,
+  updateOrderToPaid,
+  getMyOrders,
+  getOrders,
+}
--- frontend/src/screens/ProductEditScreen.js
+++ frontend/src/screens/ProductEditScreen.js
@@ -1,3 +1,4 @@
+import axios from "axios"
 import React, { useState, useEffect } from "react"
 import { Link } from "react-router-dom"
 import { Form, Button } from "react-bootstrap"
@@ -17,6 +18,8 @@ const ProductEditScreen = ({ match, history }) => {
   const [category, setCategory] = useState("")
   const [countInStock, setCountInStock] = useState(0)
   const [description, setDescription] = useState("")
+  const [uploading, setUploading] = useState(false)
+
   const dispatch = useDispatch()
 
   const productDetails = useSelector((state) => state.productDetails)
@@ -47,6 +50,29 @@ const ProductEditScreen = ({ match, history }) => {
       }
     }
   }, [dispatch, productId, product, history, successUpdate])
+
+  const uploadFileHandler = async (e) => {
+    const file = e.target.files[0]
+    const formData = new FormData()
+    formData.append("image", file)
+    setUploading(true)
+
+    try {
+      const config = {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      }
+      const { data } = await axios.post("/api/upload", formData, config)
+
+      setImage(data)
+
+      setUploading(false)
+    } catch (error) {
+      console.error(error)
+      setUploading(false)
+    }
+  }
 
   const submitHandler = (e) => {
     e.preventDefault()
@@ -107,6 +133,14 @@ const ProductEditScreen = ({ match, history }) => {
                 value={image}
                 onChange={(e) => setImage(e.target.value)}
               ></Form.Control>
+              <Form.Control
+                type='file'
+                id='image-file'
+                label='Choose file'
+                custom
+                onChange={uploadFileHandler}
+              />
+              {uploading && <Loader />}
             </Form.Group>
 
             <Form.Group controlId='brand'>
