5363ff76bc03e7005abeed78b33e5204fac04199
KaranBhut44 <karan.bhut@students.iiit.ac.in>
2022-04-18 13:34:21 +0530

    Fixed update profile bug

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -6,6 +6,8 @@ import Message from "../components/Message"
 import Loader from "../components/Loader"
 import { getUserDetails, updateUserProfile } from "../actions/userActions"
 import { listMyOrders } from "../actions/orderActions"
+import { USER_UPDATE_PROFILE_RESET } from "../constants/userConstants"
+
 const ProfileScreen = ({ location, history }) => {
   const [name, setName] = useState("")
   const [email, setEmail] = useState("")
@@ -30,7 +32,8 @@ const ProfileScreen = ({ location, history }) => {
     if (!userInfo) {
       history.push("/login")
     } else {
-      if (!user.name) {
+      if (!user || !user.name || success) {
+        dispatch({ type: USER_UPDATE_PROFILE_RESET })
         dispatch(getUserDetails("profile"))
         dispatch(listMyOrders())
       } else {
@@ -38,7 +41,7 @@ const ProfileScreen = ({ location, history }) => {
         setEmail(user.email)
       }
     }
-  }, [dispatch, history, userInfo, user])
+  }, [dispatch, history, userInfo, user, success])
 
   const submitHandler = (e) => {
     e.preventDefault()
